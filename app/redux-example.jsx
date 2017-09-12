1ffb2c6893b4c1b4061ad362b4434450872f0161
Max Nikolenko <maxim.nikolenko@gmail.com>
2017-09-13 01:10:47 +0300

    Adds reducer and starts redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,16 @@
-import redux from 'redux';
+import {createStore} from 'redux';
 
 console.log('Starting Redux example');
+
+const stateDefault = {
+	searchText: '', 
+	showCompleted: false, 
+	todos: []
+};
+const reducer = (state = stateDefault, action) => {
+	return state;
+};
+const store = createStore(reducer);
+
+const currentState = store.getState();
+console.log('currentState', currentState);
